4b632516fa8254d44b656d9e705e11f2862bf238
sso-hyeon <ashash1994@naver.com>
2022-02-02 22:53:17 +0900

    main.js pc, mb 따로 분할

---
+++ js/mb.js
@@ -0,0 +1,45 @@
+const type = new Typewriter(".tag", {
+    autoStart: true, //자동 시작
+    loop: true //반복
+});
+
+let windowWidth = window.innerWidth;
+
+window.addEventListener("resize", function () {
+    if (windowWidth >= 1200) {
+        location.href = "https://sso-hyeon.github.io/a-sh/index.html";
+    }
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        easing: "ease-in-out-sine"
+    });
+
+    let swiper;
+
+    if (768 <= windowWidth) {
+        swiper = new Swiper(".swiper-container", {
+            slidesPerView: 3,
+            autoplay: { delay: 1500, disableOnInteraction: false },
+            spaceBetween: 25,
+            loop: true
+        });
+    } else {
+        swiper = new Swiper(".swiper-container", {
+            autoplay: { delay: 1500, disableOnInteraction: false },
+            spaceBetween: 30,
+            loop: true
+        });
+    }
+
+    type.typeString("#스마일")
+        .pauseFor(600)
+        .typeString("<br>")
+        .typeString("#성실함#배려")
+        .pauseFor(600)
+        .typeString("<br>")
+        .typeString("#소통#차분함")
+        .pauseFor(1000)
+        .start();
+});
---
+++ js/pc.js
@@ -0,0 +1,35 @@
+const type = new Typewriter(".tag", {
+    autoStart: true, //자동 시작
+    loop: true //반복
+});
+
+let windowWidth = window.innerWidth;
+
+window.addEventListener("resize", function () {
+    if (windowWidth < 1200) {
+        location.href = "https://sso-hyeon.github.io/a-sh/index_m.html";
+    }
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        easing: "ease-in-out-sine"
+    });
+
+    goPortfolioPage();
+
+    type.typeString("#스마일#성실함")
+        .pauseFor(600)
+        .typeString("<br>")
+        .typeString("#배려#소통#차분함")
+        .pauseFor(1000)
+        .start();
+});
+
+function goPortfolioPage() {
+    const slide = document.querySelector(".slide");
+
+    slide.addEventListener("click", function () {
+        location.href = "https://sso-hyeon.github.io/a-sh/portfolio.html";
+    });
+}
